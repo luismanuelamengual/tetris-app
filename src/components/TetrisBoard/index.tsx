41eab4391a71fa319a363110bc5e91db8bc72fd0
Luis Amengual <luis.amengual@osanasalud.com>
2023-05-26 19:32:45 -0300

    Adjust level points counter

--- src/components/TetrisBoard/index.tsx
+++ src/components/TetrisBoard/index.tsx
@@ -8,7 +8,7 @@ interface Props {
   keyboardControls?: KeyboardControls;
 };
 
-const TETROMINOS_PER_LEVEL = 15;
+const LEVEL_POINTS_PER_LEVEL = 20;
 const ACCELERATED_INTERVAL = 50;
 const INTERVAL_PER_LEVEL = [ 1000, 900, 800, 700, 600, 500, 450, 400, 350, 300, 250, 200, 180, 160, 140, 120, 100, 80, 70, 60 ];
 
@@ -27,7 +27,7 @@ export function TetrisBoard({
   const [tetromino, setTetromino] = useState<Tetromino | null>(null);
   const [isGameOver, setIsGameOver] = useState<boolean>(false);
   const [acceleratorPressed, setAcceleratorPressed] = useState<boolean>(false);
-  const [tetrominosCounter, setTetrominosCounter] = useState<number>(0);
+  const [levelPointsCounter, setLevelPointsCounter] = useState<number>(0);
   const [level, setLevel] = useState<number>(0);
   const blockCounter = useRef<number>(0);
   const executeGameTickRef = useRef<any>();
@@ -208,7 +208,7 @@ export function TetrisBoard({
       } else {
         if (!moveTetrominoDown()) {
           freezeTetromino();
-          setTetrominosCounter(tetrominosCounter => tetrominosCounter + 1);
+          setLevelPointsCounter(points => points + 1);
         }
       }
     }
@@ -248,10 +248,8 @@ export function TetrisBoard({
   }, [onKeyDown, onKeyUp]);
 
   useEffect(() => {
-    if (tetrominosCounter > 0 && tetrominosCounter % TETROMINOS_PER_LEVEL === 0) {
-      setLevel(level => level + 1);
-    }
-  }, [tetrominosCounter]);
+    setLevel(Math.max(0, Math.ceil(levelPointsCounter / LEVEL_POINTS_PER_LEVEL) - 1));
+  }, [levelPointsCounter]);
 
   useEffect(() => {
     if (tetromino === null) {
@@ -280,9 +278,10 @@ export function TetrisBoard({
         linesMarked++;
       }
     }
-    if (linesMarked) {
+    if (linesMarked > 0) {
       setSlots(slotsMarked);
       setTimeout(() => {
+        setLevelPointsCounter(points => points + linesMarked);
         setSlots((previousSlots) => {
           const newSlots = [...previousSlots];
           let row = 19;
