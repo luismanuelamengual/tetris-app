473b646f51db064d8ef1ed99fc5209412afea3c0
Luis Amengual <luis.amengual@osanasalud.com>
2023-05-28 14:20:08 -0300

    Add buttons

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,9 +1,13 @@
-import { Page } from 'components';
+import { Button, ButtonType, Page } from 'components';
 import './index.scss';
 
 export function HomePage() {
   return (
     <Page id='home-page'>
+      <Button type={ButtonType.PRIMARY}>1P MODE</Button>
+      <Button>2P VERSUS</Button>
+      <Button>RANKINGS</Button>
+      <Button>SETTINGS</Button>
     </Page>
   );
 }
