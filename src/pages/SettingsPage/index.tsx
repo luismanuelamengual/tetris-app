a84dee8d5076bf96c24e26c229356acc0ccafd2f
Luis Amengual <luis.amengual@osanasalud.com>
2023-05-29 14:22:06 -0300

    Add buttons to settings page

--- src/pages/SettingsPage/index.tsx
+++ src/pages/SettingsPage/index.tsx
@@ -1,9 +1,15 @@
-import { Page } from 'components';
+import { goToHomePage } from 'actions';
+import { Button, ButtonType, Page } from 'components';
 import './index.scss';
 
 export function SettingsPage() {
   return (
     <Page id='settings-page'>
+      <div className='settings-form'>
+
+      </div>
+      <Button type={ButtonType.PRIMARY}>GUARDAR</Button>
+      <Button onClick={goToHomePage}>SALIR</Button>
     </Page>
   );
 }
