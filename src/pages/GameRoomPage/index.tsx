e4d62d7ea93f94807aa184931ea32e9fe7d6a9c6
Luis Amengual <luis.amengual@osanasalud.com>
2023-06-07 17:05:02 -0300

    Add states

--- src/pages/GameRoomPage/index.tsx
+++ src/pages/GameRoomPage/index.tsx
@@ -1,16 +1,21 @@
 import { goToHomePage, startNewGame } from 'actions';
 import { Button, Modal, NameInput, Page, TetrisPanel } from 'components';
+import { MatchResult } from 'models';
+import { useState } from 'react';
 import './index.scss';
 
 export function GameRoomPage() {
-  const onGameOver = () => {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [result, setResult] = useState<MatchResult | null>(null);
 
+  const onGameOver = (result: MatchResult) => {
+    setModalOpen(true);
   };
 
   return (
     <Page id='game-room-page'>
       <TetrisPanel onGameover={onGameOver}/>
-      <Modal>
+      <Modal open={modalOpen}>
         <div className='title'>Game Over</div>
         <div className='subtitle'>Enter your initials</div>
         <NameInput />
