5b0ba48d722fae36c091cd82c0820300ada1e949
Luis Amengual <luis.amengual@halopowered.com>
2023-06-10 15:17:03 -0300

    Add rankings

--- src/actions/tetris.ts
+++ src/actions/tetris.ts
@@ -1,6 +1,11 @@
 import { goToGameRoomPage } from 'actions/routing';
+import { RankingSlot } from 'models';
+import { RankingsStore } from 'stores';
 
 export function startNewGame() {
   goToGameRoomPage();
 }
 
+export function publishGameResult (slot: RankingSlot) {
+  RankingsStore.getState().addRankingSlot(slot);
+}
--- src/pages/GameRoomPage/index.tsx
+++ src/pages/GameRoomPage/index.tsx
@@ -1,26 +1,41 @@
-import { goToHomePage, startNewGame } from 'actions';
+import { goToHomePage, publishGameResult } from 'actions';
 import { Button, Modal, NameInput, Page, TetrisPanel } from 'components';
 import { MatchResult } from 'models';
 import { useState } from 'react';
+import { useSettingsStore } from 'stores';
 import './index.scss';
 
 export function GameRoomPage() {
   const [modalOpen, setModalOpen] = useState<boolean>(false);
   const [result, setResult] = useState<MatchResult | null>(null);
+  const [name, setName] = useState<string>('');
+  const keyboardControls = useSettingsStore(state => state.keyboardControls1);
 
   const onGameOver = (result: MatchResult) => {
+    setResult(result);
+    setName('');
     setModalOpen(true);
+  };
+
+  const onExitButtonClicked = () => {
+    if (result != null) {
+      publishGameResult({
+        playerName: name,
+        result: result
+      });
+    }
+    goToHomePage();
   };
 
   return (
     <Page id='game-room-page'>
-      <TetrisPanel onGameover={onGameOver}/>
+      <TetrisPanel keyboardControls={keyboardControls} onGameover={onGameOver}/>
       <Modal open={modalOpen}>
         <div className='title'>Game Over</div>
+        <div className='score'>{result?.score}</div>
         <div className='subtitle'>Enter your initials</div>
-        <NameInput />
-        <Button onClick={startNewGame}>RETRY</Button>
-        <Button onClick={goToHomePage}>EXIT</Button>
+        <NameInput value={name} onChange={setName} />
+        <Button disabled={name.length == 0} onClick={onExitButtonClicked}>EXIT</Button>
       </Modal>
     </Page>
   );
